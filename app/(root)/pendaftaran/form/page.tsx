e25f1dad3f516227dca7a3e9bc71f38ca512d11f
unknown <muhzakierbaisyas@gmail.com>
2024-06-06 20:36:36 +0700

    fix: hidden error

--- app/(root)/pendaftaran/form/page.tsx
+++ app/(root)/pendaftaran/form/page.tsx
@@ -6,7 +6,7 @@ import { useRouter } from "next/navigation";
 import { useState, useEffect } from "react";
 import { Toaster, toast } from "react-hot-toast";
 
-// Definisikan tipe data untuk Dosen
+// Define the Dosen interface
 interface Dosen {
   _id: string;
   nip: string;
@@ -22,61 +22,52 @@ const FormPendaftaran = () => {
   const [pembimbing1, setPembimbing1] = useState("");
   const [pembimbing2, setPembimbing2] = useState("");
   const [dosenList, setDosenList] = useState<Dosen[]>([]);
-  const router = useRouter();
   const [file, setFile] = useState<File | null>(null);
   const [error, setError] = useState<string | null>(null);
-
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    try {
-      const selectedFile = e.target.files ? e.target.files[0] : null;
-      setFile(selectedFile);
-      if (selectedFile) {
-        setError(null);
-      } else {
-      }
-    } catch (err) {
-      setError("Failed to load file");
-      console.error(err);
-    }
-  };
-
-  const handleRemoveFile = () => {
-    setFile(null);
-
-    setError(null);
-  };
+  const router = useRouter();
 
   useEffect(() => {
-    // Periksa apakah pengguna sudah login
     const isAuthenticated = !!localStorage.getItem("token");
 
-    // Jika belum login, arahkan ke halaman login
     if (!isAuthenticated) {
       router.push("/");
     } else {
-      // Get NIM and Nama from localStorage
       const storedNim = localStorage.getItem("nim");
       const storedNama = localStorage.getItem("userName");
       if (storedNim) setNim(storedNim);
       if (storedNama) setNama(storedNama);
 
-      // Fetch the list of advisors
-      axios
-        .get("https://siptatif-backend.vercel.app/api/dosen")
-        .then((response) => {
-          setDosenList(response.data.data);
-        })
-        .catch((error) => {
-          console.error("Error fetching dosen data:", error);
-        });
+      fetchDosenList();
     }
   }, [router]);
+
+  const fetchDosenList = async () => {
+    try {
+      const response = await axios.get(
+        "https://siptatif-backend.vercel.app/api/dosen"
+      );
+      setDosenList(response.data.data);
+    } catch (error) {
+      console.error("Error fetching dosen data:", error);
+    }
+  };
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = e.target.files ? e.target.files[0] : null;
+    setFile(selectedFile);
+    setError(selectedFile ? null : "Failed to load file");
+  };
+
+  const handleRemoveFile = () => {
+    setFile(null);
+    setError(null);
+  };
 
   const handleDaftarClick = () => {
     setShowDaftarModal(true);
   };
 
-  const handleConfirmDaftar = () => {
+  const handleConfirmDaftar = async () => {
     const formData = new FormData();
     formData.append("nim", nim);
     formData.append("nama", nama);
@@ -85,31 +76,25 @@ const FormPendaftaran = () => {
     formData.append("pembimbing_1", pembimbing1);
     formData.append("pembimbing_2", pembimbing2);
 
-    axios
-      .post("https://siptatif-backend.vercel.app/api/ta", formData)
-      .then((response) => {
-        console.log("Pendaftaran berhasil:", response.data);
-        // Tampilkan toast sukses
-        toast.success("Pendaftaran berhasil!", {
-          style: {
-            backgroundColor: "white",
-            color: "green",
-          },
-        });
-        setTimeout(() => {
-          router.push("/pendaftaran");
-        }, 2000);
-      })
-      .catch((error) => {
-        console.error("Error submitting pendaftaran:", error);
-        // Tampilkan toast error
-        toast.error("Pendaftaran gagal!", {
-          style: {
-            backgroundColor: "white",
-            color: "red",
-          },
-        });
+    try {
+      await axios.post("https://siptatif-backend.vercel.app/api/ta", formData);
+      toast.success("Pendaftaran berhasil!", {
+        style: {
+          backgroundColor: "white",
+          color: "green",
+        },
       });
+      setTimeout(() => {
+        router.push("/pendaftaran");
+      }, 2000);
+    } catch (error) {
+      toast.error("Pendaftaran gagal!", {
+        style: {
+          backgroundColor: "white",
+          color: "red",
+        },
+      });
+    }
 
     setShowDaftarModal(false);
   };
@@ -120,6 +105,7 @@ const FormPendaftaran = () => {
 
   return (
     <>
+      <Toaster />
       <form>
         {/* Card */}
         <div className="p-4 bg-white border shadow rounded-xl sm:p-7 mb-8">
@@ -140,7 +126,7 @@ const FormPendaftaran = () => {
                 type="text"
                 value={nim}
                 className="block w-full px-3 py-2 text-sm border border-gray-200 rounded-lg shadow-sm pe-11  disabled:opacity-50 disabled:pointer-events-none"
-                required
+                onChange={(e) => setNim(e.target.value)}
               />
             </div>
 
@@ -152,8 +138,8 @@ const FormPendaftaran = () => {
               <input
                 type="text"
                 value={nama}
+                onChange={(e) => setNama(e.target.value)}
                 className="block w-full px-3 py-2 text-sm border border-gray-200 rounded-lg shadow-sm pe-11  disabled:opacity-50 disabled:pointer-events-none"
-                required
               />
             </div>
 
@@ -168,7 +154,6 @@ const FormPendaftaran = () => {
                 onChange={(e) => setJudul(e.target.value)}
                 className="block w-full px-3 py-2 text-sm border border-gray-200 rounded-lg shadow-sm pe-11  disabled:opacity-50 disabled:pointer-events-none"
                 placeholder="Enter your title"
-                required
               />
             </div>
 
@@ -181,9 +166,8 @@ const FormPendaftaran = () => {
                 value={kategori}
                 onChange={(e) => setKategori(e.target.value)}
                 className="block w-full px-3 py-2 text-sm border border-gray-200 rounded-lg shadow-sm pe-9  disabled:opacity-50 disabled:pointer-events-none"
-                required
               >
-                <option selected>Pilih kategori</option>
+                <option>Pilih kategori</option>
                 <option>Laporan</option>
                 <option>Paper Based</option>
               </select>
@@ -198,9 +182,8 @@ const FormPendaftaran = () => {
                 value={pembimbing1}
                 onChange={(e) => setPembimbing1(e.target.value)}
                 className="block w-full px-3 py-2 text-sm border border-gray-200 rounded-lg shadow-sm pe-9  disabled:opacity-50 disabled:pointer-events-none"
-                required
               >
-                <option selected>Pilih pembimbing 1</option>
+                <option>Pilih pembimbing 1</option>
                 {dosenList.map((dosen) => (
                   <option key={dosen._id} value={dosen.nama}>
                     {dosen.nama}
@@ -219,7 +202,7 @@ const FormPendaftaran = () => {
                 onChange={(e) => setPembimbing2(e.target.value)}
                 className="block w-full px-3 py-2 text-sm border border-gray-200 rounded-lg shadow-sm pe-9  disabled:opacity-50 disabled:pointer-events-none"
               >
-                <option selected>Pilih pembimbing 2</option>
+                <option>Pilih pembimbing 2</option>
                 {dosenList.map((dosen) => (
                   <option key={dosen._id} value={dosen.nama}>
                     {dosen.nama}
@@ -248,7 +231,7 @@ const FormPendaftaran = () => {
                   viewBox="0 0 16 16"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M7.646 5.146a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 6.707V10.5a.5.5 0 0 1-1 0V6.707L6.354 7.854a.5.5 0 1 1-.708-.708l2-2z"
                   />
                   <path d="M4.406 3.342A5.53 5.53 0 0 1 8 2c2.69 0 4.923 2 5.166 4.579C14.758 6.804 16 8.137 16 9.773 16 11.569 14.502 13 12.687 13H3.781C1.708 13 0 11.366 0 9.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383zm.653.757c-.757.653-1.153 1.44-1.153 2.056v.448l-.445.049C2.064 6.805 1 7.952 1 9.318 1 10.785 2.23 12 3.781 12h8.906C13.98 12 15 10.988 15 9.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 4.825 10.328 3 8 3a4.53 4.53 0 0 0-2.941 1.1z" />
@@ -322,12 +305,14 @@ const FormPendaftaran = () => {
                 </div>
                 <div className="flex justify-center">
                   <button
+                    type="button"
                     className="px-6 py-3 mr-4 text-sm font-medium text-gray-700 bg-gray-100 rounded-md hover:bg-gray-200 "
                     onClick={handleCancelDaftar}
                   >
                     Cancel
                   </button>
                   <button
+                    type="button"
                     className="px-6 py-3 text-sm font-medium text-white bg-gradient-to-l from-teal-400 to-sky-500 hover:from-teal-500 hover:to-sky-600 rounded-md  "
                     onClick={handleConfirmDaftar}
                   >
@@ -339,7 +324,6 @@ const FormPendaftaran = () => {
           </div>
         )}
       </form>
-      <Toaster />
     </>
   );
 };

a84456fca5ec8ebe4c5b517a6bfe3c28bf2c3764
unknown <muhzakierbaisyas@gmail.com>
2024-06-27 17:45:55 +0700

    fix: approved status

--- app/(root)/status/page.tsx
+++ app/(root)/status/page.tsx
@@ -156,7 +156,7 @@ const Status = () => {
                             className={`py-1 px-1.5 inline-flex items-center gap-x-1 text-xs font-medium rounded-full ${
                               item.status === "Ditolak"
                                 ? "bg-red-100 text-red-800"
-                                : item.status === "Diterima"
+                                : item.status === "Disetujui"
                                 ? "bg-teal-100 text-teal-800"
                                 : "bg-orange-100 text-orange-800"
                             }`}
@@ -172,7 +172,7 @@ const Status = () => {
                               {item.status === "Ditolak" && (
                                 <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"></path>
                               )}
-                              {item.status === "Diterima" && (
+                              {item.status === "Disetujui" && (
                                 <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
                               )}
                               {item.status === "Diproses" && (
@@ -284,7 +284,7 @@ const Status = () => {
                   </a>
                 </div>
               </div>
-              {viewDetail.status === "Diterima" && (
+              {viewDetail.status === "Disetujui" && (
                 <>
                   <div className="grid grid-cols-[120px_1fr] gap-4 border-t border-green-500 pt-4">
                     <div className="text-gray-500">Penguji 1</div>

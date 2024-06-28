a36abde54ccd3ef23955b480242098e7f26d9d60
unknown <muhzakierbaisyas@gmail.com>
2024-06-28 08:32:27 +0700

    ed: final fixed

--- app/(root)/pendaftaran/page.tsx
+++ app/(root)/pendaftaran/page.tsx
@@ -185,7 +185,7 @@ const Pendaftaran = () => {
                             className={`py-1 px-1.5 inline-flex justify-center items-center gap-x-1 text-xs font-medium rounded-full ${
                               p.status === "Ditolak"
                                 ? "bg-red-100 text-red-800"
-                                : p.status === "Diterima"
+                                : p.status === "Disetujui"
                                 ? "bg-teal-100 text-teal-800"
                                 : "bg-orange-100 text-orange-800"
                             }`}
@@ -201,7 +201,7 @@ const Pendaftaran = () => {
                               {p.status === "Ditolak" && (
                                 <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"></path>
                               )}
-                              {p.status === "Diterima" && (
+                              {p.status === "Disetujui" && (
                                 <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
                               )}
                               {p.status === "Diproses" && (
